45d4dc5095de492f7523444fd1393d595a35b9d4
Abrilam <abril.morenomaltez@alu.frt.utn.edu.ar>
2020-12-07 00:06:41 -0300

    Modificacion, ver porfa

--- veterinaria.c
+++ veterinaria.c
@@ -2,10 +2,9 @@
 #include<stdlib.h>
 #include<conio.h>
 #include<string.h>
-//holis
 
 /*Modulo de Administracion: 
-	*Alta baja veterinario sistemaa
+	*Alta veterinario sistema
 	*Borrar al veterinario del sistema, junto a sus datos
 	*borrar al empleado del sistema
 	*Dar de alta a masocta ya curada
@@ -23,15 +22,18 @@ modulo consultorio:
 	el vetereniario ingresa con su numero de matricula y contraseña 
 	se habre un menu de opciones:
 		1. llama por apellido y nombre a la mascota que atendera 
-		    *poner estado evolucion de la mascota maximo 380* (historia clinica, comun a todos)
-		    se debe dejar registro del veterinario que escribio el estado de evolucion junto a la fecha
-			*se borra la mascota del listado*/
+		 *poner estado evolucion de la mascota maximo 380
+		 * (historia clinica, comun a todos)
+		  se debe dejar registro del veterinario que escribio el estado de evolucion junto a la fecha
+	   	*se borra la mascota del listado*/
+			
+////============================================================FUNCIONES Y ESTRUCTURAS============================================================
 
 struct administrador
 {
 	char apenom[100];
 	char contra[100];
-	bool borrado=false; // siempre vamos a agregar el campo borrado porque a este campo lo vamos a utilizar para hacer borrados logicos
+	//bool borrado=false; // siempre vamos a agregar el campo borrado porque a este campo lo vamos a utilizar para hacer borrados logicos
     			            // el borrado logico es como cuando se borra un archivo y se va la papelera, el borrado fisico es cuando ya se borra lo que hay en la papelera xd
 };
 
@@ -45,15 +47,30 @@ struct paciente
 	float peso;
 };
 
+struct veterinarios
+{
+	char nombre[100];
+	char apellido[100];
+	char usuario[100];
+	char contra[100];
+};
+
 void administracion(administrador admi);
 void recepcionista(paciente mascota);
-
-
+//void atencionvet();
+void regvet(FILE*altaveterinarios,veterinarios altav); 
+//============================================================PRINCIPAL============================================================
 main()
 {
 	int opcion	;
 	administrador admi;
 	paciente mascota;
+	veterinarios altav;
+	FILE *altaveterinarios;
+	
+
+	
+		
 	
 	do
 	{
@@ -91,15 +108,19 @@ main()
 	 	
  	
 		
-	}while(opcion!=4)
-
+	}
 	
 	
 	
 	
 
-}		
-void administracion(administrador admi)
+	}while(opcion!=4);
+	
+	
+}
+
+//============================================================ADMINISTRACION============================================================	
+void administracion(administrador admi,FILE*altaveterinarios,veterinarios altav)
 {	
 	int opcion;
 	printf("\nIngrese su nuevo usuario: ");
@@ -126,7 +147,7 @@ do
 	 scanf("%d", &opcion);
 	 switch(opcion)
 	 {
-	 	case 1: //regvet(); 
+	 	case 1: regvet(altaveterinarios,altav); 
 	 	printf("1.- Registrar Veterinario\n");
 	 	system("pause");
 	 	break;
@@ -163,6 +184,35 @@ do
 
 }
 
+void regvet(FILE*altaveterinarios,veterinarios altav)
+{
+	altaveterinarios=fopen("altaveterinarios.dat","a+b");
+	if(altaveterinarios==NULL)
+	{
+	printf("no se pudo abrir el archivo");
+	 }
+	 
+	printf("Ingrese el Nombre del profesional");
+	gets(altav.nombre);
+
+	printf("Ingrese el Apellido "); 
+	gets(altav.apellido);
+	
+	printf("Ingrese su usuario para el inicio de sesion");
+	gets(altav.usuario);
+	
+	printf("Ingrese su contraseña");
+	gets(altav.contra);
+	
+	
+	fwrite(&altav,sizeof(veterinarios),1,altaveterinarios);
+	
+	fclose(altaveterinarios);
+}
+
+
+
+//============================================================RECEPCION============================================================
 void recepcionista(paciente mascota)
 {
 	int opcion;
@@ -218,3 +268,57 @@ void recepcionista(paciente mascota)
 		
 		
 }
+
+
+//============================================================VETERINARIO============================================================
+/*void atencionvet()
+{
+	
+	do
+	{
+			printf("\nModulo Consultorio Veterinario\n");
+			printf("\n =========================\n");
+			printf("\n 1.- Iniciar Sesión\n");
+			printf("\n 2.- Visualizar Lista de Espera de Turnos (informe)\n");
+			printf("\n 3.- Registrar Evolución de la Mascota\n");
+			printf("\n 4.- Cerrar la aplicación.\n");
+			printf("\n Ingrese una opción: _ \n");
+			scanf("%d",&opcion);
+			
+	
+		
+		switch(opcion)
+	 {
+	 	case 1:// iniciosesion(); 
+	 	
+	 	system("pause");
+	 	break;
+	 	
+	 	case 2: //espera_mascota();
+	 	
+	 	system("pause");
+	 	break;
+	 	
+	 	case 3: //evolucionmascota();
+	 	
+	 		system("pause");
+	 	break;
+	 	
+	 	case 4:	printf("\nGracias por utilizar nuestros servicios.");
+	 	
+	 
+	 	system("pause");
+	 	break;
+	 	
+	
+	 	default:
+		printf("Ingreso una opcion incorrecta, ingrese nuevamente");
+		break;
+	 		
+	 }
+ 
+	}while(opcion!=4);
+
+		
+}	
+	*/
